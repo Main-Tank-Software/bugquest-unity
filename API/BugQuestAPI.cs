90bfd378e61f9961bd0fcf2c9597572cff13353a
bqbuild <support@bugquest.gg>
2026-02-03 04:02:24 +0000

    CI Auto-update Unity package from build artifact

--- API/BugQuestAPI.cs
+++ API/BugQuestAPI.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 
 public class BugQuestAPI : MonoBehaviour
 {
+    public static bool StartVideoClip()
+    {
+        return BugQuest.Instance.Video.StartManualClip();
+    }
+    public static bool StopVideoClip()
+    {
+        return BugQuest.Instance.Video.StopManualClip();
+    }
+
+    public static bool IsVideoClipRecording()
+    {
+        return BugQuest.Instance.Video.IsManualClipRecording();
+    }
     public static void Disable()
     {
         BugQuest.Disable();
